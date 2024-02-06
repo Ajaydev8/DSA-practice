be6dc35540b872cafaf1aa4ce8899b9c3a740d27
Ajaydev8 <131373049+Ajaydev8@users.noreply.github.com>
2024-02-06 15:22:19 +0530

    how to create a variable and display it on the console

---
+++ variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+  // first practice
+  // int x; //Declaration
+  // x = 5; //assigment
+
+  // std::cout << x; //Displaying
+
+  // second practice
+  
+
+  return 0;
+}
