44f697bcbd39a90fbcc13f2bdaffead067da8999
Ajay Mishra <ajaymishraa1112@gmail.com>
2024-03-11 10:46:46 +0530

    namespace definition

---
+++ namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main() {
+    // Namespace = provide a solution for preventing name conflicts
+    //             in large projects. Each entity needs a unique name.
+    //             A namespace allows for identically named entities
+    //             as long as the namespaces are different.
+
+    return 0;
+}
