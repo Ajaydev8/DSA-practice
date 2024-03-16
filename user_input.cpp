7715736e8cc38816fd53102575dd1c0a2c53999f
Ajay Mishra <ajaymishraa1112@gmail.com>
2024-03-16 16:37:46 +0530

    how to eliminate new line character

--- user_input.cpp
+++ user_input.cpp
@@ -8,12 +8,13 @@ int main()
     std::string name;
     int age;
 
-    std::cout << "What's your full name?: ";
-    
-    // std::cin >> name;// <-------- // this is where name will be stored
-
     std::cout << "What's your age?: ";
     std::cin >> age; // this is wher age will be stored
+
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin >> std::ws, name); // this code gets line which means it takes the space between them also.
+
+    // std::cin >> name;// <-------- // this is where name will be stored, The only problem is this does not take spaces in consideration.
 
     std::cout << "Hello " << name << '\n';
     std::cout << "You are " << age << " years old.";
