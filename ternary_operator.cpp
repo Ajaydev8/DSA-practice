8ca0ac9c9d54b6f86a717da09642216fe755453f
Ajay Mishra <ajaymishraa1112@gmail.com>
2024-03-27 15:29:26 +0530

    initial commit, defination

---
+++ ternary_operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+    // ternary operator ?: = replacement to an if/else statement
+    // condition ? expressiona1 : expression2;
+
+     
+
+    return 0;
+}
