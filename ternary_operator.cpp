381b50afa4163cb838ca3b722c9575d5cc13de41
Ajay Mishra <ajaymishraa1112@gmail.com>
2024-03-27 15:41:43 +0530

    ternary operator done

--- ternary_operator.cpp
+++ ternary_operator.cpp
@@ -2,10 +2,37 @@
 
 int main(void)
 {
-    // ternary operator ?: = replacement to an if/else statement
-    // condition ? expressiona1 : expression2;
 
-     
+    // ternary operator ?: = replacement to an if/else statement
+    // condition ? expressiona1(if true) : expression2(if false);
+
+    //first exercise *************************************
+
+    int grade;
+
+     std::cout << "Enter grade between (1-100): ";
+     std::cin >> grade;
+
+    // writing in ternary condition style 
+    (grade >= 60) ? std::cout << "You Pass!" : std::cout << "You fail!";
+    //this really saves us some key strokes.
+
+    //******************************************************
+
+    //second exercise *****************************************
+
+    int number = 9;
+
+    number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
+
+    //*********************************************************
+
+    //third execise *******************************************
+
+    bool hungry = true;
+
+    // hungry ? std::cout << "You are hungry" : std::cout << "You are full";
+    std::cout << (hungry ? "You are hungry" : "You are full."); // this is another way of writing ternary condition
 
     return 0;
 }
