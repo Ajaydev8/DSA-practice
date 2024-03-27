7c4a8cdfb63fc3c399e08eaa8f51b71f0117e01b
Ajay Mishra <ajaymishraa1112@gmail.com>
2024-03-27 17:26:50 +0530

    learned while loop in c++

--- while_loops.cpp
+++ while_loops.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
+    // While loop: while loop is like if statement but we can
+    //             intentionally repeat some code with it.
 
+    std::string name;
+
+    // until the condition given in the parantheses is true the
+    // the code will execute itself over and over again.
+    while(name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
+
+    std::cout << "Hello " << name;
 
     return 0;
 }
